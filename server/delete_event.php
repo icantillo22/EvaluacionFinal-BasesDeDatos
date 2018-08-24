2a4cf7d516c45aedfc7264f7c082c71eaddcee60
Isaac Cantillo <31421368+icantillo22@users.noreply.github.com>
2018-08-24 12:37:19 -0400

    Commit #5

--- server/delete_event.php
+++ server/delete_event.php
@@ -1,5 +1,37 @@
 <?php
 
+  require('conexion.php');
+
+  session_start();
+
+  $con = new ConectorBD('localhost', 'root', '');
+
+  $response['conexion'] = $con->initConexion('agenda');
+
+  if (isset($_SESSION['user'])) {
+
+    if ($response['conexion'] == 'OK') {
+
+      $userActivo = $_SESSION['user'];
+      $idEvento = $_POST['id'];
+
+      if ($con->eliminarRegistro('eventos', 'user_id = '.$userActivo.' AND id = '.$idEvento)) {
+        $response['msg'] = 'OK';
+      }else {
+        $response['msg'] = 'Se produjo un error en la eliminacion';
+      }
+
+      echo json_encode($response);
+      $con->cerrarConexion();
+
+
+    }else {
+      echo 'Se presento un error en la conexion';
+    }
+
+  }else {
+    echo "No ha iniciado sesion";
+  }
 
 
  ?>
--- server/logout.php
+++ server/logout.php
@@ -1,4 +1,12 @@
 <?php
-
-
- ?>
+	session_start();
+	if (isset($_SESSION['user'])) {
+		session_destroy();
+		$response['msg'] = 'Redireccionar';
+    header('Location: http://localhost/EvaluacionFinal-BasesDeDatos/client/');
+	}else{
+		$response['msg'] = 'Redireccionar';
+    header('Location: http://localhost/EvaluacionFinal-BasesDeDatos/client/');
+	}
+	echo json_encode($response);
+?>
